5429825f1c9627513f7b8720df8edca4ada716dd
QGazQ <email@qgazq.net>
2019-04-17 23:26:48 +0100

    Split the output to seperate files and seperate segments so they won't limit the size or cause confusion Left in my shim comment for future testing locally

--- novice.js
+++ novice.js
@@ -1,6 +1,15 @@
 'use strict';
 const { ScreepsAPI } = require('screeps-api')
 const fs = require('fs').promises
+/*
+// Node 8 shim for local testing
+const {promisify} = require('util');
+const fsr = require('fs')
+let fs={};
+fs.readFile = promisify(fsr.readFile);
+fs.readdir = promisify(fsr.readdir);
+fs.writeFile = promisify(fsr.writeFile);
+*/
 
 /*
 Two output formats
@@ -9,29 +18,28 @@ Two output formats
 * Second a set of novice and respawnAreas indexed by timestamp
 	Useful to find a set of the other rooms in an area (use the First format from a room to get the type and timestamp then index into the other)
 
-Could be argued we don't need to provide the second type, the user can do it if they want
+Could be argued we don't need to provide the second type, the user can do it if they want.
+Therefore its in seperate json files and an optional second segment
 
-const outputFormat = {
-	rooms:{
-		room1Name:{ [novice/respawnArea]:timestampX },
-		room2Name:{ [novice/respawnArea]:timestampY },
-		room3Name:{ [novice/respawnArea]:timestampZ }
+const outputFormat1 = {
+	room1Name:{ [novice/respawnArea]:timestampX },
+	room2Name:{ [novice/respawnArea]:timestampY },
+	room3Name:{ [novice/respawnArea]:timestampZ }
+};
+
+const outputFormat2 = {
+	novice:{
+		timestamp1: [room1Name, room2Name, room3Name],
+		timestamp2: [room4Name, room5Name, room6Name]
 	},
-
-	areas:{
-		novice:{
-			timestamp1: [room1Name, room2Name, room3Name],
-			timestamp2: [room4Name, room5Name, room6Name]
-		},
-		respawnArea:{
-			timestamp3: [room7Name, room8Name, room9Name]
-		}
+	respawnArea:{
+		timestamp3: [room7Name, room8Name, room9Name]
 	}
 };
 */
 
 async function run() {
-  const [,,server,segment] = process.argv;
+  const [,,server,segment,segmentArea] = process.argv;
   const api = await ScreepsAPI.fromConfig(server);
   const files = await fs.readdir('.');
   const shardFiles = files.filter(f => f.endsWith('.roominfo.json'))
@@ -40,10 +48,15 @@ async function run() {
     const [shard] = file.split('.')
     const data = JSON.parse(await fs.readFile(file, 'utf8'))
     const raw=parseZones(data);
-    await fs.writeFile(`${shard}.novice.json`, raw)
+    await fs.writeFile(`${shard}.novice.json`, raw[0])
+    await fs.writeFile(`${shard}.novice.area.json`, raw[1])
     if(server && segment) {
       console.log(`Uploading to segment ${segment} on ${shard} of ${server}`)
-      await api.memory.segment.set(+segment, raw, shard)
+      await api.memory.segment.set(+segment, raw[0], shard)
+    }
+    if(server && segmentArea) {
+      console.log(`Uploading to segment ${segmentArea} on ${shard} of ${server}`)
+      await api.memory.segment.set(+segmentArea, raw[1], shard)
     }
   }
   console.log('Done!')
@@ -86,21 +99,22 @@ function parseZones(data)
 	}
 
 	console.log("Rooms:",Object.keys(nnRooms).length,"Novice areas:",Object.keys(areas.novice).length,"Respawn areas:",Object.keys(areas.respawnArea).length);
+	let asStringR= JSON.stringify(nnRooms);
 	//console.log(JSON.stringify(nnRooms, null, 2));
+	if (asStringR.length>100000)
+	{
+		console.log("Size too big",asStringR.length,"setting fallback error");
+		asStringR= JSON.stringify({error:true});
+	}
+	let asStringA= JSON.stringify(areas);
 	//console.log(JSON.stringify(areas, null, 2));
-	let asStringC= JSON.stringify({rooms:nnRooms,areas:areas});
-	if (asStringC.length>100000)
+	if (asStringA.length>100000)
 	{
-		console.log("Combined size too big",asStringC.length,"removing areas");
-		asStringC= JSON.stringify({rooms:nnRooms,areas:{}});
+		console.log("Size too big",asStringA.length,"setting fallback error");
+		asStringA= JSON.stringify({error:true});
 	}
-	if (asStringC.length>100000)
-	{
-		console.log("Size too big",asStringC.length,"setting fallback");
-		asStringC= JSON.stringify({rooms:{},areas:{}});
-	}
-	console.log("Output size", asStringC.length);
+	console.log("Output sizes, Rooms:", asStringR.length, "Areas:", asStringA.length);
 
-	return asStringC;
+	return [asStringR,asStringA];
 }
 
